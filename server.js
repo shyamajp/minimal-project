6d6d243de13c4bb6aeb29b5f796397be6c9d1ab5
Shiori Yamazaki <shiori.yamazaki.jp@gmail.com>
2023-09-22 23:17:12 +0300

    add postgres with docker-compose

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require("express");
+const { Pool, Client } = require("pg");
 
 // Constants
 const PORT = 8080;
@@ -10,6 +11,20 @@ const HOST = "0.0.0.0";
 const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+// Database
+const client = new Client({
+  user: "user",
+  host: "postgres",
+  database: "db",
+  password: "pass",
+  port: 5432,
+});
+client.connect();
+client.query("SELECT NOW()", (err, res) => {
+  console.log("Error or response:: ", err, res);
+  client.end();
 });
 
 app.listen(PORT, HOST, () => {
