55f72d6c794d2cb8967a7487d41934dce36ab90f
Shiori Yamazaki <shiori.yamazaki.jp@gmail.com>
2023-09-22 23:48:22 +0300

    use postgres queries

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const { Pool, Client } = require("pg");
 // Constants
 const PORT = 8080;
 const HOST = "0.0.0.0";
+const people = [
+  { name: "Aaron", age: 10 },
+  { name: "Brian", age: 20 },
+  { name: "Chris", age: 30 },
+  { name: "David", age: 40 },
+  { name: "Elvis", age: 50 },
+];
 
 // App
 const app = express();
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
 // Database
 const client = new Client({
@@ -22,9 +26,48 @@ const client = new Client({
   port: 5432,
 });
 client.connect();
-client.query("SELECT NOW()", (err, res) => {
-  console.log("Error or response:: ", err, res);
-  client.end();
+
+app.get("/", (req, res) => {
+  res.status(200).send("ok");
+});
+
+app.get("/initialize", (req, res) => {
+  client.query(
+    `DROP TABLE IF EXISTS person;` +
+      `CREATE TABLE person  (
+    id serial,
+    name varchar(10) NOT NULL,
+    age integer NOT NULL
+  )`,
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      res.status(204).send("Created table 'person'");
+    }
+  );
+});
+
+app.get("/populate", (req, res) => {
+  client.query(
+    "INSERT INTO person (name, age) VALUES" +
+      people.map((person) => ` ('${person.name}', ${person.age})`).join(","),
+    (error, result) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).send(`Inserted ${result.rowCount} people`);
+    }
+  );
+});
+
+app.get("/users", (req, res) => {
+  client.query("SELECT * FROM person;", (error, result) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).send(result.rows);
+  });
 });
 
 app.listen(PORT, HOST, () => {
